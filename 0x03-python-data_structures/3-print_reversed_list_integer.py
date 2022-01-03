1ed35aea0f374d79ec2be1a84421450b87aa112c
RayBar72 <email = 3963@holbertonschool.com>
2022-01-03 08:16:28 -0800

    Uno

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    news = [list for list in reversed(my_list)]
-    for new in news:
-        print("{:d}".format(new))
+    if my_list:
+        news = [list for list in reversed(my_list)]
+        for new in news:
+            print("{:d}".format(new))
