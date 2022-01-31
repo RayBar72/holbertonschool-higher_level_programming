5573f00fd7c91601d06e0025ae483a4bf7c2112d
RayBar72 <3963@holbertonschool.com>
2022-01-31 15:39:49 -0800

    Cambio en impresion

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,15 +5,21 @@ Class BaseGometry
 
 
 class BaseGeometry:
-    """Class that contains a function area()"""
+    """
+    Class that contains a function area()
+    """
 
     def area(self):
-        """Function that is not implemented and rise exception"""
+        """
+        Function that is not implemented and rise exception
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Function that validates value"""
+        """
+        Function that validates value
+        """
         if type(value) is not int:
-            raise TypeError(name + " must be an integer")
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(name + " must be greater than 0")
+            raise ValueError("{:s} must be greater than 0".format(name))
