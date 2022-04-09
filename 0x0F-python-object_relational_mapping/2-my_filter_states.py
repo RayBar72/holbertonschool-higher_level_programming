a098f3748e918e8e654424cf537ef472ad894b97
RayBar72 <3963@holbertonschool.com>
2022-04-09 09:24:15 -0300

    Fixing Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC"
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY \
+                '{}' ORDER BY id ASC"
                 .format(argv[4]))
 
     query_rows = cur.fetchall()
