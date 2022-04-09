a175cd09514c24696768db717db3db07f36db7cb
RayBar72 <3963@holbertonschool.com>
2022-04-09 09:20:50 -0300

    Task one fixing codestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY\
+                'N%' ORDER BY id ASC")
 
     query_rows = cur.fetchall()
 
