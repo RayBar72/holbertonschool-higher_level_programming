63bc9c907a0d3f226f79f3592f9967767872ebad
RayBar72 <3963@holbertonschool.com>
2022-04-22 19:59:35 -0300

    Task 6. Using sort

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,4 +7,5 @@ Function that finds a peak in a list of unsorted integers
 def find_peak(list_of_integers):
     """Finds the peak of a list"""
     if list_of_integers:
-        return max(list_of_integers)
+        list_of_integers.sort()
+        return list_of_integers[-1]
