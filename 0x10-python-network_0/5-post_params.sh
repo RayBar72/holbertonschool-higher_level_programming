8d2ee39a9b2d75d59c86f123c3ebf28eddc4a2f2
RayBar72 <3963@holbertonschool.com>
2022-04-22 15:37:20 -0300

    Task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that ends a POST request to the passed URL
-cur -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
