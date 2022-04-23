59ecaf10df20d1175487f0f91888d8daa15e7753
RayBar72 <3963@holbertonschool.com>
2022-04-23 15:43:20 -0300

    Fixing pep8

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,8 @@ import requests
 def main():
     """Main funtion that fetches status"""
     r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}".format(type(r.text), r.text))
+    print("Body response:\n\t- type: {}\n\t- content: {}"
+          .format(type(r.text), r.text))
 
 
 if __name__ == "__main__":
