f272b98bc73bd4e6081e068ea18ddf3fb20fab2c
RayBar72 <3963@holbertonschool.com>
2022-05-17 13:37:48 -0300

    Up to task 3. Changing format 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,8 +4,8 @@
 const axios = require('axios').default;
 axios.get(process.argv[2])
   .then(function (response, error) {
-    console.log('code: ' + response.status);
+    console.log('code:', response.status);
   })
   .catch(function (error) {
-    console.log('code ' + error.response.status);
+    console.log('code', error.response.status);
   });
